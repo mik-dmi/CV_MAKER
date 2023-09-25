4e583b65c7f422cae6b46f22a617fe8f8261eaeb
mik-dmi <122227240+mik-dmi@users.noreply.github.com>
2023-09-25 02:48:03 +0100

    General Box form se made an styles

--- src/component/general.jsx
+++ src/component/general.jsx
@@ -1,4 +1,5 @@
-import { useState } from "react"
+import { useState, useEffect, useRef } from "react"
+import ChevronDown from './icons/chevron_down.svg'
 
 
 export function General({userData}){
@@ -7,6 +8,20 @@ export function General({userData}){
         phoneNumber: "",
         email: ""
       })
+
+    const [toggle, setToggle] = useState(false)
+    const [heightEl, setHeighEl] = useState();
+
+    const refHeight = useRef()
+
+    useEffect(() => { 
+      setHeighEl(`${refHeight.current.scrollHeight}px`)
+
+    }, [])
+
+    const toggleState = () => {
+      setToggle(!toggle)
+    }
 
     function handleSubmit(e){
         e.preventDefault()
@@ -30,39 +45,55 @@ export function General({userData}){
     
     return(
         
-      <div className='general_forms_contianer'>
-        <div className="forms_header">General</div>
-        <form onSubmit={handleSubmit}  className="forms" >
-          <div className="label_input_container">
-            <label>Full Name:</label> 
-            <input 
-            type="text"
-            name="fullName"
-                value={newInput.fullName}
-                onChange={handleInputChange}
-                /> 
-          </div>
-          <div className="label_input_container">
-            <label>Phone Number:</label> 
-            <input 
-                type="tel"    
-                name = "phoneNumber"
-                value={newInput.phoneNumber}
-                onChange={handleInputChange}
-                /> 
-          </div>
-          <div className="label_input_container">
-            <label>Email:</label>
-            <input 
-              name = "email"
-                value={newInput.email}
-                onChange={handleInputChange}
-                type="email" /> 
-          </div>
-              <div className="submit_forms_button">
-                  <button className="submit_button">ADD</button>
-              </div>
-        </form>
+      <div className='general_forms_container'>
+          <button 
+          onClick={toggleState}
+          className="general_forms_button">
+            <span className="title_forms">General</span>
+            <img src = {ChevronDown} 
+            className={`imgChev ${toggle && 'active'}`}
+
+            
+            />
+          </button>
+        <div 
+        style={{height: toggle ? `${heightEl}` : "0px"}}
+        className={toggle ? "accordion_toggle animated" : "accordion_toggle"}
+        ref={refHeight}>
+          <form onSubmit={handleSubmit}  className="forms" ref={refHeight} aria-hidden = {toggle ? "true" : "false"} >
+            <div className="label_input_container"
+            >
+              <label>Full Name:</label> 
+              <input 
+              type="text"
+              name="fullName"
+                  value={newInput.fullName}
+                  onChange={handleInputChange}
+                  /> 
+            </div>
+            <div className="label_input_container">
+              <label>Phone Number:</label> 
+              <input 
+                  type="tel"    
+                  name = "phoneNumber"
+                  value={newInput.phoneNumber}
+                  onChange={handleInputChange}
+                  /> 
+            </div>
+            <div className="label_input_container">
+              <label>Email:</label>
+              <input 
+                name = "email"
+                  value={newInput.email}
+                  onChange={handleInputChange}
+                  type="email" /> 
+            </div>
+                <div className="submit_forms_button">
+                    <button className="submit_button">ADD</button>
+                </div>
+          </form>
+        </div>
+      
       </div>
     ) 
 }
