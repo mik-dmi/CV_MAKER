85c4562b99bf1b74ba5150eec59f7660674cbfed
mik-dmi <122227240+mik-dmi@users.noreply.github.com>
2023-10-14 03:41:25 +0100

    The display got  some css styling applied.

--- src/component/cv_display.jsx
+++ src/component/cv_display.jsx
@@ -5,9 +5,9 @@ export function CvDisplay({generalData,educationData, professionalData}){
 
     return(
         <div className='right_side_of_body'>
-        <header>
+        <header className="header_container">
           <h1>{generalData.fullName}</h1>
-          <ul>
+          <ul className="contact_info">
             <li>
                 <i className='bx bxs-phone'>{generalData.phoneNumber}</i>
             </li>
@@ -18,45 +18,40 @@ export function CvDisplay({generalData,educationData, professionalData}){
                 <i className='bx bxl-linkedin-square'>{generalData.linkedIn}</i>
             </li>
           </ul>
-
         </header>
-  
+
         <section id="education">
           <h2>Education</h2>
-          {educationData.map((education, index)=>(
-          <div key={index*5} className="education_section">
-            <h3>{education.institution}</h3>
-          
-            <ul>
-                <div className="place_and_date_container"> 
-                    <h4>{education.degree}</h4> 
-                    <h4>{education.graduationDate}</h4> 
-                </div>
-                <li>{education.academicDescription}</li>
-            </ul>
+          <div className="education_container">
+            {educationData.map((education, index)=>(
+            <div key={index*5} className="individual_education_section">
+              <h3>{education.degree}</h3>
+              <ul className = "education_details">
+                <li className = "education_degree">{education.institution}</li>
+                <li className = "education_graduation">{education.graduationDate}</li>
+                <li className = "bullet_point education_description">{education.academicDescription}</li>
+              </ul>
+            </div>  
+            ))} 
           </div>
-        ))}  
         </section>
 
         <section id="experience">
           <h2>Professional Experience</h2>
-          {professionalData.map((professional, index)=>(
-          <div key={index*5} className="professional_section">
-            <h3>{professional.positionName}</h3>
-          
-            <ul>
-                <div className="place_and_date_container"> 
-                    <h4>{professional.company}</h4> 
-                    <h4>{professional.location}</h4> 
-                </div>
-                <div>
-                  <h4>{professional.startDate} - {professional.endDate}</h4>
-                
-                </div>
-                <li>{professional.professionalDescription}</li>
-            </ul>
+          <div className="experience_container">
+            {professionalData.map((professional, index)=>(
+            <div key={index*5} className="professional_section">
+              <h3>{professional.company}</h3>
+              <ul className="professional_details"> 
+                  <li className="professional_company">{professional.positionName}</li>
+                  <li className="professional_location">{professional.location}</li>
+                  <li className="professional_dates">{professional.startDate} - {professional.endDate}</li>
+                  <li className="bullet_point professional_description">{professional.professionalDescription}</li>
+              </ul>
+            </div>
+            ))} 
           </div>
-        ))}  
+        
         </section>
   
       </div>
